d27ee34d8ba68016cc4d2cc1177a9d3458b8c3b0
cinthiadepaula <cinthiade.paula@outlook.com>
2020-09-18 21:06:37 -0300

    Updates

---
+++ precache-manifest.99acd9441e988d70f5c7f52d0d9a0bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa90fb1df20e5bb767bada3a5f75f8e",
+    "url": "/calc_react/index.html"
+  },
+  {
+    "revision": "f658155c1d31f87006ee",
+    "url": "/calc_react/static/css/main.897752d8.chunk.css"
+  },
+  {
+    "revision": "9bdd7c94ff03ab6f4efe",
+    "url": "/calc_react/static/js/2.ddc226f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc_react/static/js/2.ddc226f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f658155c1d31f87006ee",
+    "url": "/calc_react/static/js/main.446ecaee.chunk.js"
+  },
+  {
+    "revision": "8afdd0a455c3d308d4a6",
+    "url": "/calc_react/static/js/runtime-main.cb79fbd7.js"
+  },
+  {
+    "revision": "7cb58857d294ac1e09b72ea9403c690a",
+    "url": "/calc_react/static/media/RobotoMono-Thin.7cb58857.ttf"
+  }
+]);
